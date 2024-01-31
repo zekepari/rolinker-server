3919bfe9e0a46232fc17c9a00669e097823a2730
Zeke Pari <pariezekiel@gmail.com>
2024-02-01 02:30:52 +1000

    new routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
 import Router from 'koa-router';
 import * as authController from '../controllers/authController.js';
+import authenticate from '../middlewares/authenticate.js';
 
 const router = new Router();
 
 router.get('/auth/discord/login', authController.discordLogin);
 router.get('/auth/discord/callback', authController.discordCallback);
+router.get('/auth/roblox/login', authenticate, authController.robloxLogin);
+router.get('/auth/roblox/callback', authenticate, authController.robloxCallback);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,5 +6,7 @@ const router = new Router();
 
 router.get('/users/@me', authenticate, userController.getCurrentUser);
 router.get('/users/:userId/accounts', userController.getUserAccounts);
+router.delete('/users/:userId/accounts', authenticate);
+router.post('/users/:userId/accounts/primary', authenticate);
 
 export default router;
