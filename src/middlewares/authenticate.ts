97983c00762e3a58c8ad758cb49f4e85b2a5f9f6
Zeke Pari <pariezekiel@gmail.com>
2024-02-01 02:30:45 +1000

    refactor

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,24 +1,25 @@
 import { Context, Next } from "koa";
-import { getDatabase } from "../database/mongo.js";
 import jwt from "jsonwebtoken";
+import { getDatabase } from "../database/mongo.js";
 
 export default async (ctx: Context, next: Next) => {
-    const db = getDatabase();
-    const usersCollection = db.collection('users');
     const token = ctx.cookies.get('token');
 
     if (!token) {
         ctx.state.user = null;
-        await next();
-        return;
+        
+        return await next();
     }
-  
+
+    const db = getDatabase();
+    const usersCollection = db.collection('users');
+
     try {
-      const { sub } = await jwt.verify(token, process.env.JWT_SECRET as string);
-      ctx.state.user = await usersCollection.findOne({ _id: sub as any });
+        const { sub } = await jwt.verify(token, process.env.JWT_SECRET as string);
+        ctx.state.user = await usersCollection.findOne({ _id: sub as any });
     } catch (e) {
-      ctx.state.user = null;
+        ctx.state.user = null;
     }
-  
+
     await next();
 }
