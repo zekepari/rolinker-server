0de9371428f2cf80ee63da4ae8a267b179025e68
Zeke Pari <pariezekiel@gmail.com>
2024-02-01 02:29:55 +1000

    remove logic that depended on legacy

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,14 +1,10 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
-import { createAuthUrl } from "../oauth.js";
-import { authComponent, authEmbed } from "../messages.js";
 
 export default {
     data: new SlashCommandBuilder()
         .setName('link')
         .setDescription('link your roblox and discord account together'),
     async execute(interaction: CommandInteraction) {
-        const authUrl = createAuthUrl(interaction.user.id);
-
-        await interaction.reply({embeds: [authEmbed], components: [authComponent(authUrl)], ephemeral: true});
+        console.log('hi')
     },
 };
