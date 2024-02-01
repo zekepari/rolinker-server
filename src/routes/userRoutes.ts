b209d321bff0a35eab47e4cee6cc5d0071af0c37
Zeke Pari <pariezekiel@gmail.com>
2024-02-01 13:37:06 +0000

    planned routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,9 +4,16 @@ import authenticate from '../middlewares/authenticate.js';
 
 const router = new Router();
 
+//@me (authenticated)
 router.get('/users/@me', authenticate, userController.getCurrentUser);
-router.get('/users/:userId/accounts', userController.getUserAccounts);
-router.delete('/users/:userId/accounts', authenticate);
-router.post('/users/:userId/accounts/primary', authenticate);
+router.delete('/users/@me/accounts/:accountId', authenticate, userController.deleteAccount);
+router.post('/users/@me/accounts/primary/:accountId', authenticate, userController.setPrimaryAccount);
+
+//open
+router.get('/users/:userId') //return basic discord user information - build the information based on the refresh token
+router.get('/users/:userId/accounts', userController.getUserAccounts); //return roblox account ids and primary id
+router.get('/users/:userId/servers') //return discord servers in which the user owns and rolinker is a member of
+
+//only non-get routes should be authenticated for security.
 
 export default router;
